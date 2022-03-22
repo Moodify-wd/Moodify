7e03df346c4ed23f3ebd7592a9de43ef87cd1e27
KelanLindahl <48532604+KelanLindahl@users.noreply.github.com>
2022-03-21 21:29:24 -0400

    Mood  form listener

--- public/mood.js
+++ public/mood.js
@@ -1,4 +1,8 @@
 function moodSelector(){
-   let userMood=document.getElementById("mood");
+    var pickedMood=document.getElementById("moodValue");
+    var userMood=pickedMood.options[pickedMood.selectedIndex].value;
     console.log("Your selected mood was: " + userMood);
+    if (userMood=="happy"){
+        alert("Variable works");
+    }
 }
